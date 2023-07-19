8a22ba24a69a3c1966f3fbcc905df726c2f77082
Dominion1212 <dominionogbaji15@gmail.com>
2023-07-19 20:04:17 +0100

    login phase commit

---
+++ screens/Login.js
@@ -0,0 +1,161 @@
+import React, { useState, useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+// formik
+import { Formik } from 'formik';
+
+import {
+  StyledContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledInputLabel,
+  StyledFormArea,
+  StyledButton,
+  StyledTextInput,
+  LeftIcon,
+  RightIcon,
+  InnerContainer,
+  ButtonText,
+  MsgBox,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+  Colors,
+} from './../components/styles';
+import { View, ActivityIndicator, ScrollView } from 'react-native';
+
+//colors
+const { darkLight, brand, primary } = Colors;
+
+// icon
+import { Octicons, Fontisto, Ionicons } from '@expo/vector-icons';
+
+
+
+
+
+const Login = ({ navigation }) => {
+    const [hidePassword, setHidePassword] = useState(true);
+    const [message, setMessage] = useState();
+    const [messageType, setMessageType] = useState();
+    const [googleSubmitting, setGoogleSubmitting] = useState(false);
+
+    
+
+
+  
+    return (
+     <ScrollView>
+    
+        <StyledContainer>
+            <StatusBar style="dark" />
+            <InnerContainer>
+            <PageLogo resizeMode="cover" source={require('./../assets/img/expo-bg1.png')} />
+            <PageTitle>Flower Crib</PageTitle>
+            <SubTitle>Account Login</SubTitle>
+            <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={(values, { setSubmitting }) => {
+              if (values.email == '' || values.password == '') {
+                handleMessage('Please fill in all fields');
+                setSubmitting(false);
+              } else {
+                handleLogin(values, setSubmitting);
+              }
+            }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values, isSubmitting }) => (
+              <StyledFormArea>
+                <MyTextInput
+                  label="Email Address"
+                  placeholder="andyj@gmail.com"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                  icon="mail"
+                />
+                <MyTextInput
+                  label="Password"
+                  placeholder="* * * * * * * *"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  secureTextEntry={hidePassword}
+                  icon="lock"
+                  isPassword={true}
+                  hidePassword={hidePassword}
+                  setHidePassword={setHidePassword}
+                />
+                <MsgBox type={messageType}>{message}</MsgBox>
+
+                {!isSubmitting && (
+                  <StyledButton onPress={handleSubmit}>
+                    <ButtonText>Login</ButtonText>
+                  </StyledButton>
+                )}
+                {isSubmitting && (
+                  <StyledButton disabled={true}>
+                    <ActivityIndicator size="large" color={primary} />
+                  </StyledButton>
+                )}
+
+                <Line />
+
+                {!googleSubmitting && (
+                  <StyledButton onPress={handleSubmit} google={true}>
+                    <Fontisto name="google" size={25} color={primary} />
+                    <ButtonText google= {true} >Sign in with Google</ButtonText>
+                  </StyledButton>
+                )}
+                {googleSubmitting && (
+                  <StyledButton disabled={true} google={true}>
+                    <ActivityIndicator size="large" color={primary} />
+                  </StyledButton>
+                )}
+
+                 <ExtraView>
+                  <ExtraText>Don't have an account already? </ExtraText>
+                  <TextLink onPress={() => navigation.navigate('Signup')}>
+                    <TextLinkContent>Signup</TextLinkContent>
+                  </TextLink>
+                </ExtraView>
+
+              
+              </StyledFormArea>
+            )}
+          </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    </ScrollView>
+
+    );
+}
+    
+const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
+    return (
+        <View>
+          <LeftIcon>
+            <Octicons name={icon} size={30} color={brand} />
+          </LeftIcon>
+          <StyledInputLabel>{label}</StyledInputLabel>
+          <StyledTextInput {...props} />
+          {isPassword && (
+            <RightIcon
+              onPress={() => {
+                setHidePassword(!hidePassword);
+              }}
+            >
+              <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} />
+            </RightIcon>
+          )}
+        </View>
+      );
+    };
+    
+export default Login;
---
+++ screens/Signup.js
@@ -0,0 +1,240 @@
+import React, { useState, useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+// formik
+import { Formik } from 'formik';
+
+import {
+  StyledContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledInputLabel,
+  StyledFormArea,
+  StyledButton,
+  StyledTextInput,
+  LeftIcon,
+  RightIcon,
+  InnerContainer,
+  ButtonText,
+  MsgBox,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+  Colors,
+} from './../components/styles';
+import { View, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
+
+//colors
+const { darkLight, brand, primary } = Colors;
+
+// icon
+import { Octicons, Fontisto, Ionicons } from '@expo/vector-icons';
+
+// Datetimepicker
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+
+
+
+
+
+
+const Signup = ({ navigation }) => {
+    const [hidePassword, setHidePassword] = useState(true);
+    const [show, setShow] = useState(false);
+    const [date, setDate] = useState(new Date(2000, 0, 1));
+    const [message, setMessage] = useState();
+    const [messageType, setMessageType] = useState();
+    const [googleSubmitting, setGoogleSubmitting] = useState(false);
+
+    
+     // Actual value to be sent
+  const [dob, setDob] = useState();
+
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(false);
+    setDate(currentDate);
+    setDob(currentDate);
+  };
+
+  const showDatePicker = () => {
+    setShow('True');
+  };
+
+    
+
+
+  
+    return (
+     <ScrollView>
+    
+        <StyledContainer>
+            <StatusBar style="dark" />
+            <InnerContainer>
+            <PageLogo resizeMode="cover" source={require('./../assets/img/expo-bg1.png')} />
+            <PageTitle>Flower Crib</PageTitle>
+            <SubTitle>Account Signup</SubTitle>
+            {show && (
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode="date"
+              is24Hour={true}
+              display="default"
+              onChange={onChange}
+              style={{
+                backgroundColor: 'yellow',
+              }}
+            />
+          )}
+
+
+            <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={(values, { setSubmitting }) => {
+              if (values.email == '' || values.password == '') {
+                handleMessage('Please fill in all fields');
+                setSubmitting(false);
+              } else {
+                handleLogin(values, setSubmitting);
+              }
+            }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values, isSubmitting }) => (
+              <StyledFormArea>
+                 <MyTextInput
+                  label="Full Name"
+                  placeholder="Richard Barnes"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('name')}
+                  onBlur={handleBlur('name')}
+                  value={values.name}
+                  icon="person"
+                />
+                <MyTextInput
+                  label="Email Address"
+                  placeholder="andyj@gmail.com"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                  icon="mail"
+                />
+                <MyTextInput
+                  label="Date of Birth"
+                  placeholder="YYYY - MM - DD"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('dateOfBirth')}
+                  onBlur={handleBlur('dateOfBirth')}
+                  value={dob ? dob.toDateString() : ''}
+                  icon="calendar"
+                  editable={false}
+                  isDate={true}
+                  showDatePicker={showDatePicker}
+                />
+                <MyTextInput
+                  label="Password"
+                  placeholder="* * * * * * * *"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  secureTextEntry={hidePassword}
+                  icon="lock"
+                  isPassword={true}
+                  hidePassword={hidePassword}
+                  setHidePassword={setHidePassword}
+                />
+                <MyTextInput
+                  label="Confirm Password"
+                  placeholder="* * * * * * * *"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('confirmPassword')}
+                  onBlur={handleBlur('confirmPassword')}
+                  value={values.confirmPassword}
+                  secureTextEntry={hidePassword}
+                  icon="lock"
+                  isPassword={true}
+                  hidePassword={hidePassword}
+                  setHidePassword={setHidePassword}
+                />
+                <MsgBox type={messageType}>{message}</MsgBox>
+
+                {!isSubmitting && (
+                  <StyledButton onPress={handleSubmit}>
+                    <ButtonText>Signup</ButtonText>
+                  </StyledButton>
+                )}
+                {isSubmitting && (
+                  <StyledButton disabled={true}>
+                    <ActivityIndicator size="large" color={primary} />
+                  </StyledButton>
+                )}
+
+                <Line />
+
+                {!googleSubmitting && (
+                  <StyledButton onPress={handleSubmit} google={true}>
+                    <Fontisto name="google" size={25} color={primary} />
+                    <ButtonText google= {true} >Sign in with Google</ButtonText>
+                  </StyledButton>
+                )}
+                {googleSubmitting && (
+                  <StyledButton disabled={true} google={true}>
+                    <ActivityIndicator size="large" color={primary} />
+                  </StyledButton>
+                )}
+
+                 <ExtraView>
+                  <ExtraText>Don't have an account already? </ExtraText>
+                  <TextLink onPress={() => navigation.navigate('Signup')}>
+                    <TextLinkContent>Signup</TextLinkContent>
+                  </TextLink>
+                </ExtraView>
+
+              
+              </StyledFormArea>
+            )}
+          </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    </ScrollView>
+
+    );
+}
+    
+const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, isDate, showDatePicker, ...props }) => {
+    return (
+        <View>
+        <LeftIcon>
+          <Octicons name={icon} size={30} color={brand} />
+        </LeftIcon>
+        <StyledInputLabel>{label}</StyledInputLabel>
+  
+        {isDate && (
+          <TouchableOpacity onPress={showDatePicker}>
+            <StyledTextInput {...props} />
+          </TouchableOpacity>
+        )}
+        {!isDate && <StyledTextInput {...props} />}
+  
+        {isPassword && (
+          <RightIcon
+            onPress={() => {
+              setHidePassword(!hidePassword);
+            }}
+          >
+            <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} />
+          </RightIcon>
+        )}
+      </View>
+      );
+    };
+    
+export default Signup;
